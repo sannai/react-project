6b2324c9a225a412489f2b197b5104f0fa8365f7
zzp <965005909@qq.com>
2019-10-22 18:03:18 +0800

    项目搭建

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { MobXProviderContext } from 'mobx-react'
+import { createGlobalStyle } from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+const GlobalStyle = createGlobalStyle`
+    html,
+    body,
+    #root {
+        height: 100%;
+    }
+    body {
+        margin: 0;
+    }
+    ul {
+        padding: 0;
+        margin: 0;
+    }
+    a {
+        text-decoration: none;
+    }
+    li {
+        list-style-type: none;
+    }
+`
+
+ReactDOM.render(
+    <>
+        <MobXProviderContext.Provider value={1}>
+            <App />
+        </MobXProviderContext.Provider>
+        <GlobalStyle whiteColor />
+    </>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
